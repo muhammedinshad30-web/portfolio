ac6a2ae48fc04ce1aa9f4e6cb6b8468b41904f63
muhammedinshad30-web <muhammedinshad30@gmail.com>
2025-09-18 19:36:24 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function sendmail(){
+    var params ={
+        name: document.getElementById("name").value ,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+
+
+const serviceID ="service_kvjz02d";
+const templateID="template_awy1ftb"
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{
+        document.getElementById("name").value="";
+        document.getElementById("email").value="";
+        document.getElementById("message").value="";
+        console.log(res);
+        alert("your message send is successfully");
+
+    }
+)
+.catch(err=>console.log(err));
+}
